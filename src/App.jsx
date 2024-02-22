be58f6dc5760250cb9240c76a71e453329f9db2a
Noomi <kykkanenn@gmail.com>
2024-02-22 18:50:49 +0200

    07- Tiedon välitys komponenttien välillä

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}</p>
     </div>
   );
 };
@@ -10,10 +10,9 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
 
-      <Hello />
-      <Hello />
+      <Hello name="Maya" />
+      <Hello name="Pekka" />
     </div>
   );
 };
