18128f0a0f391548cf14fe175721a87714a6f735
Noomi <kykkanenn@gmail.com>
2024-02-23 13:07:54 +0200

    09- Älä renderöi olioita ja tehtävä on valmis

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,21 @@ const Hello = (props) => {
 };
 
 const App = () => {
-  const nimi = "Pekka";
-  const ika = 10;
+  const friends = [
+    { name: "Leevi", age: 4 },
+    { name: "Venla", age: 10 },
+  ];
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+      <p>
+        {friends[0].name} {friends[0].age}
+      </p>
+      <p>
+        {friends[1].name} {friends[1].age}
+      </p>
     </div>
   );
 };
+
 export default App;
